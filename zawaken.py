58fdf3050972bacdf216d7211e3305d597770f60
Zawaken <zawaken@outlook.com>
2017-12-07 22:01:44 +0100

    Sweet

--- zawaken.py
+++ zawaken.py
@@ -5,8 +5,15 @@ import discord
 from discord.ext import commands
 from data import token
 from lxml import html
+import requests
+import secrets
+import json
+
 
 bot = commands.Bot(command_prefix='>>', description='xd')
+bitcoin_price_url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+data = requests.get(bitcoin_price_url).json()
+price_in_usd = data['bpi']['USD']['rate']
 
 @bot.event
 async def on_ready():
@@ -25,5 +32,13 @@ async def flip(ctx):
     """stuff"""
     flip = random.choice(['heads', 'tails'])
     await bot.say(flip)
+
+@bot.command(pass_context=True)
+async def btc(ctx):
+    """A command to get BTC price in USD"""
+    await bot.say('BTC price is currently at $' + price_in_usd + ' USD')
+
+@bot.command(pass_context=True)
+async def 
 
 bot.run(token)
