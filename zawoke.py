582f4f11994ba2f84370a24143187f6543e8a1a2
Zawaken <zawaken@outlook.com>
2017-12-10 22:46:33 +0100

    muddayakka

--- zawoke.py
+++ zawoke.py
@@ -6,6 +6,7 @@ import requests
 # import secrets
 # import json
 import config as con
+import time
 import discord
 from discord.ext import commands
 # from lxml import html
@@ -65,11 +66,15 @@ async def joke(cmd, message, args):
 @bot.command(pass_context=True)
 async def info(ctx):
     """stuff"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     mcontent = ctx.message.content
     if mcontent == con.prefix + 'info':
-        await bot.say('Name = ' + ctx.message.author.mention)
+        await bot.say('Name: ' + ctx.message.author.name + '\n'
+                      'Id: ' + ctx.message.author.id)
     elif con.prefix + 'info <@' in mcontent:
-        await bot.say('Name = ' + mcontent.replace(con.prefix + 'info', ''))
+        await bot.say('Name: ' + mcontent.replace(con.prefix + 'info', '') + '\n' +
+                      'Id: ' + mcontent.replace(con.prefix + 'info <@', '').replace('>', ''))
+        await bot.delete_message(ctx.message)
     else:
         await bot.say('How the hell do you expect me to find info if the info isn\'t readable?')
 
@@ -77,6 +82,7 @@ async def info(ctx):
 @bot.command(pass_context=True)
 async def hug(ctx):
     """stuff"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     mcontent = ctx.message.content
     if mcontent == con.prefix + 'hug':
         await bot.say('*If ' + ctx.message.author.name +
@@ -87,21 +93,26 @@ async def hug(ctx):
     else:
         await bot.say(ctx.message.author.mention + ' hugged ' +
                       mcontent.replace(con.prefix + 'hug', '') + ' :heart:')
+        await bot.delete_message(ctx.message)
 
 @bot.command(pass_context=True)
 async def kys(ctx):
     """ Kill yourself """
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     mcontent = ctx.message.content
     if mcontent == con.prefix + 'kys':
         print('command.kys :: no argument')
+        await bot.say('kys, input an argument next time you stoobid')
     else:
         await bot.say('Hey' + mcontent.replace(con.prefix +'kys', '') + ', ' +
                       ctx.message.author.name +
                       ' wants you to die a wonderful death inflicted by yourself!')
+        await bot.delete_message(ctx.message)
 
 @bot.command(pass_context=True)
 async def kms(ctx):
     """stuff"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     await bot.say('kys ' + ctx.message.author.mention +
                   '\nsuicide hotline = https://www.wikihow.com/Tie-a-Noose')
 
@@ -109,6 +120,7 @@ async def kms(ctx):
 @bot.command(pass_context=True)
 async def mprint(ctx):
     """stuff"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     mcontent = ctx.message.content
     await bot.say(mcontent.replace(con.prefix + 'mprint', ''))
 
@@ -116,37 +128,59 @@ async def mprint(ctx):
 @bot.command(pass_context=True)
 async def poke(ctx):
     """Poke"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     mcontent = ctx.message.content
     if mcontent == con.prefix + 'poke':
         await bot.say('OwO wat dis, is it a poking? I am not feel it')
     else:
         await bot.say(ctx.message.author.name + ' poked you, ' +
                       mcontent.replace(con.prefix + 'poke', ''))
+        await bot.delete_message(ctx.message)
 
-
+"""
 @bot.command()
 async def fuck(context, arg):
-    """stuff"""
+    \"""stuff\"""
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     await context.send(arg)
-
+"""
 
 @bot.command(pass_context=True)
-async def addbtc(ctx):
+async def addbtc(ctx, create_custom_emoji, server : discord.Server = None):
     """ I want to fix this hahah u thought i was gon say die lol """
-    await ctx.create_custom_emoji(server='', name='btc', image='btc.png')
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
+    await create_custom_emoji(ctx.message.Server, name='btc', image='btc.png')
     await bot.say('I am finnish! Perkele')
 
 
 @bot.command(pass_context=True)
 async def test(ctx):
     """ tester """
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     await bot.say(ctx.message.mention.name)
 
 @bot.command(pass_context=True)
-async def eagames():
+async def eagames(ctx):
     """ Buy our new expansion pack for 200$ to get darth vader """
+    print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
     await bot.say('```Electronic Arts is a ... \n\nTo see the rest of this message paypal' +
                   'me $20k \n Zawaken@waifu.club```')
+
+@bot.command(pass_context=True)
+async def purge(ctx):
+    """ Deletes the messages of the specified user. """
+    print(ctx.message.author.name + ' ' + ctx.message.author.id + ' ' + ctx.message.content)
+    mcont = ctx.message.content
+    if mcont == str(con.prefix + 'purge'):
+        await bot.delete_message(ctx.message)
+        print('command.purge :: no argument')
+    elif mcont == con.prefix + 'purge all':
+        await bot.send_message(ctx.message.channel, 'Clearing messages...')
+        time.sleep(2)
+        async for msg in bot.logs_from(ctx.message.channel):
+            await bot.delete_message(msg)
+    else:
+        print('error')
 
 
 bot.run(con.token)
