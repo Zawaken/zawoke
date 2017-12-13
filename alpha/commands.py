96cd00bd73b78acbe8447ae73a32349990fd5a41
Zawaken <zawaken@outlook.com>
2017-12-13 15:44:19 +0100

    fuck ctx

--- alpha/commands.py
+++ alpha/commands.py
@@ -13,11 +13,5 @@ class Cmds:
         """stuff"""
         await self.bot.say('Pong')
 
-    @commands.command(pass_context=True)
-    async def test(self, bot, ctx):
-        """ tester """
-        print(ctx.message.author.name + ctx.message.author.id + ctx.message.content)
-        await bot.say(ctx.message.mention.name)
-
 def setup(bot):
     bot.add_cog(Cmds(bot))
