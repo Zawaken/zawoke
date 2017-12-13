a8edc161ed50418722e9a344498a61fa9195b817
Zawaken <zawaken@outlook.com>
2017-12-13 15:52:36 +0100

    heck

--- alpha/commands.py
+++ alpha/commands.py
@@ -12,6 +12,14 @@ class Cmds:
     async def ping(self):
         """stuff"""
         await self.bot.say('Pong')
+    
+    @commands.command(pass_context=True)
+    async def flip(self, ctx):
+        coinflip = random.choice(['heads', 'tails'])
+        if coinflip == 'heads':
+            await self.bot.add_reaction(ctx.message, '⚪')
+        else:
+            await self.bot.add_reaction(ctx.message, '⚫')
 
 def setup(bot):
     bot.add_cog(Cmds(bot))
