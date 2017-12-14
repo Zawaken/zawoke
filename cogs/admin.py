798efcfd08afb1f1530aca4f00e3bdb4db61a49b
Zawaken <zawaken@outlook.com>
2017-12-14 13:32:33 +0100

    lol

--- cogs/admin.py
+++ cogs/admin.py
@@ -22,21 +22,30 @@ class Admin:
             await self.bot.say('Permission Denied')
 
     @commands.command(pass_context=True)
-    async def purge(self, ctx, messages: int):
+    async def purge(self, ctx):
         """ Deletes the messages of the specified user. """
         print(ctx.message.author.name + ' ' + ctx.message.author.id + ' ' + ctx.message.content)
+        userid = ctx.message.author.id
         mcont = ctx.message.content
         if mcont == con.prefix + 'purge':
             await self.bot.delete_message(ctx.message)
             print('command.purge :: no argument')
-        elif mcont == con.prefix + 'purge all':
+        elif mcont == con.prefix + 'purge all' and userid == con.owner_id or userid in str(con.dev_id):
             await self.bot.send_message(ctx.message.channel, 'Clearing messages...')
             asyncio.sleep(2)
             async for msg in self.bot.logs_from(ctx.message.channel):
                 await self.bot.delete_message(msg)
         else:
             print('error')
-
+            await self.bot.say('Permission Denied')
+"""
+    @commands.command(pass_context=True)
+    async def clear(self, ctx):
+        mcont = ctx.message.content
+        mcontr = mcont.replace(con.prefix + 'clear', '')
+        #async for mcontr in self.bot.logs_from(ctx.message.channel):
+            #await self.bot.delete_message(int(mcontr))
+"""
 def setup(bot):
     """Makes it so Cogs actually work"""
     bot.add_cog(Admin(bot))
